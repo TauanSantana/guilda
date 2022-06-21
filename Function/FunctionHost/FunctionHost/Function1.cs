0b6ed5bdfbe4addfef0a63e8892e573721982dd4
CETI - Tauan Santana da Silva <2838@appai.org.br>
2022-06-21 12:48:07 -0300

    rabbitmq

--- Function/FunctionHost/FunctionHost/Function1.cs
+++ Function/FunctionHost/FunctionHost/Function1.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net;
+using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
 
 namespace FunctionHost
 {
@@ -20,9 +23,38 @@ namespace FunctionHost
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string responseMessage = $"Azure Function Host: { Dns.GetHostName()}";
+            string responseMessage = $"Azure Function Host: { Dns.GetHostName()} - {DateTime.UtcNow.AddHours(-3)}";
 
+            var factory = new ConnectionFactory()
+            {
+                HostName = Environment.GetEnvironmentVariable("RabbitMq_host", EnvironmentVariableTarget.Process),
+                UserName = Environment.GetEnvironmentVariable("RabbitMq_user", EnvironmentVariableTarget.Process),
+                Password = Environment.GetEnvironmentVariable("RabbitMq_pass", EnvironmentVariableTarget.Process)
+            };
+
+            
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var props = channel.CreateBasicProperties();
+                props.ContentType = "text/plain";
+                props.Headers = new Dictionary<string, object>();
+
+                channel.QueueDeclare(queue: "queue-guilda",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicPublish(exchange: "",
+                                        routingKey: "queue-guilda",
+                                        basicProperties: props,
+                                        body: Encoding.UTF8.GetBytes(responseMessage));
+
+            }
             return new OkObjectResult(responseMessage);
+           
         }
     }
 }
